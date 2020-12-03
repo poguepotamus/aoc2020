583725ac1f4d19bbc0c84380e8a6ae6ad293bc74
poguepotamus <matthewpogue606@gmail.com>
2020-12-03 15:54:08 -0600

    yet another main file

--- aoc2020/__main__.py
+++ aoc2020/__main__.py
@@ -3,32 +3,33 @@
 # STD LIB
 from sys import argv as args
 from pathlib import Path
+from glob import glob
 
-if __name__ == '__main__':
-	args = ['', 2, 'inputFiles/day2_test']
-
-# Our script requires two arguments,
-#	Day to solve
-#	Input filepath
-# Lets check for those now
-if len(args) < 3:
-	raise ValueError('''Must both the day to solve and the input file
-
-	USAGE:
-		aoc2020 [day] [inputFile]
+def dayReport(day, inputFile=None):
+	# Default input file and day string
+	inputFile = Path(f'inputFiles/day{day}') if inputFile is None else inputFile
+	day = f'Day{day}'
+	# Finding the module and class for our day, creating an instance
+	solution = getattr(__import__(day), day)( Path(inputFile) )
+	# Everything is in order, we print our results
+	print(f'''# Day {day} Report:
+		Part 1: {solution.part1()}
+		Part 2: {solution.part2()}
 	''')
 
-# Now checking if we have the input file
-if not Path(str(args[2])).exists():
-	raise FileNotFoundError(f'`{args[2]}` was not found, please provide a valid input file')
+# If we have three arguments, then we're given a specific day and file to run
+if len(args) == 3:
+	# Now checking if we have the input file
+	if not Path(str(args[2])).exists():
+		raise FileNotFoundError(f'`{args[2]}` was not found, please provide a valid input file')
 
-# Looks in order, lets find which class to create
-day = f'Day{args[1]}'
-# Finding the module and class for our day, creating an instance
-solution = getattr(__import__(day), day)( Path(args[2]) )
+	# Looks in order, lets find which class to create
+	dayReport(args[1], args[2])
 
-# Everything is in order, we'll find the value by giving our filename to our object solver
-print(f'''# Day {args[1]} Report:
-	Part 1: {solution.part1()}
-	Part 2: {solution.part2()}
-''')
+# Otherwise, we're going to give every report
+else:
+	for day in [file.split('.')[0][-1] for file in glob('aoc2020/Day*.py')]:
+		dayReport(day)
+
+
+
